44309c91f2c264c4f8c3ca1abffffb1d9ef90ac8
Rex <rex@Silverbook.local>
2025-12-03 15:41:22 +1100

    added a placeholder to update the database just by uploading the excel file

--- database_interaction/main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-df = pd.read_excel("membership.xlsx")
-engine = create_engine("postgresql://rex@localhost:5432/DDSC")
-
-df.to_sql("members", engine, if_exists="replace")
